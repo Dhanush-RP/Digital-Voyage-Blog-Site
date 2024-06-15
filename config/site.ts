c23e273a25f1dbd8aeb07d7defe4bafe3f91e1d6
Dhanush R P <rpdhanush15@gmail.com>
2024-06-15 13:57:47 +0530

    blog-app-init

---
+++ config/site.ts
@@ -0,0 +1,11 @@
+export const siteConfig = {
+    name: "My Blo",
+    url: "https://example.com",
+    description: "NextJs Blog",
+    author: "Dhanush",
+    links: {
+        github: "https://github.com/Dhanush-RP"
+    }
+}
+
+export type SiteConfig = typeof siteConfig
